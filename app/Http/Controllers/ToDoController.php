8e609dabc25eca6a39ba23b690074ec69025e059
Mikelis <ipb23.m.trekse@vtdt.edu.lv>
2025-03-09 01:32:37 +0200

    Vajag

---
+++ app/Http/Controllers/ToDoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ToDo;
+class ToDoController extends Controller
+{
+    public function index()
+    {
+        $todos = ToDo::all();
+        return view("todos.index", compact("todos"));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ToDoController;
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/iphone', function () {
     return view('why');
 });
-
+Route::get('/todos', [ToDoController::class, 'index']);
 
